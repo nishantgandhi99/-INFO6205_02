8f37d48f2685af340cac0898ab044ef03d7dc9ff
nishantgandhi99 <nishantgandhi99@gmail.com>
2017-12-07 04:38:48 -0500

    UnitTest for Graph Generator

--- src/test/java/edu/neu/gc/AppTest.java
+++ src/test/java/edu/neu/gc/AppTest.java
@@ -1,6 +1,7 @@
 package edu.neu.gc;
 
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
@@ -14,5 +15,21 @@ public class AppTest {
     public void testFitness() {
         ColorSet mySet = new ColorSet();
         assertEquals(mySet.getFitness(), 0);
+    }
+
+    @Test
+    public void testGraphGenerator() throws GraphGenerator.InvalidEdgeCountException {
+        int vertexCount = 7;
+        int edgeCount = 5;
+        GraphGenerator graph1 = new GraphGenerator(vertexCount, edgeCount);
+        assertEquals(vertexCount, graph1.getVertexCount());
+        assertNotEquals(vertexCount + 1, graph1.getVertexCount());
+    }
+
+    @Test(expected=GraphGenerator.InvalidEdgeCountException.class)
+    public void testGraphGenerator1() throws GraphGenerator.InvalidEdgeCountException {
+        int vertexCount = 7;
+        int edgeCount = vertexCount*vertexCount;
+        GraphGenerator graph2 = new GraphGenerator(vertexCount, edgeCount);
     }
 }
